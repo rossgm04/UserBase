c2ef73ec797d581dff7b86c5318d4083547ea7df
RocioGM <rociogarciamendez2004@gmail.com>
2025-05-07 20:22:54 +0200

    feat(Start): New project

---
+++ UserBase/src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'UserBase'; 
+}
